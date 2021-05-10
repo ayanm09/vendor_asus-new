ab5c2efa185f40e918634ff09fdae44d34dcce38
AruAA <arora.aryan7@gmail.com>
2021-05-10 16:54:17 +0200

    Remove hotword enrollment apps
    Signed-off-by: Aryan Arora <arora.aryan7@gmail.com>

--- Z01R/Z01R-vendor.mk
+++ Z01R/Z01R-vendor.mk
@@ -1209,8 +1209,6 @@ PRODUCT_PACKAGES += \
     IWlanService \
     TimeService \
     PowerOffAlarm \
-    HotwordEnrollmentOKGoogleWCD9340 \
-    HotwordEnrollmentXGoogleWCD9340 \
     QtiTelephonyService \
     uceShimService \
     WfdService \
